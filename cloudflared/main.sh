2030874e4fded37d2c53d6a077b26b32d1d5824c
sheldon <sheldonxxx@pm.me>
2023-03-30 14:14:22 +0800

    test

--- cloudflared/main.sh
+++ cloudflared/main.sh
@@ -10,7 +10,16 @@ if [ "${CF_TOKEN}" = "quick" ]; then
     for var in "${ports[@]}"; do
         local metrics_port=$((var+1))
         nohup cloudflared tunnel --url http://localhost:$var --metrics localhost:$metrics_port --pidfile /tmp/cloudflared_$var.pid > /tmp/cloudflared_$var.log 2>&1 &
-        mhostname=$(curl http://localhost:$metrics_port/quicktunnel | jq -r '.hostname')
+        while true; do
+            response=$(curl -s http://localhost:$metrics_port/quicktunnel)
+            if [ $? -eq 0 ]; then
+                hostname=$(echo "$response" | jq -r '.hostname')
+                echo "Success! Hostname is $hostname"
+                break
+            fi
+            echo "Failed to get response. Retrying in 5 seconds..."
+            sleep 5
+        done
         echo $mhostname
     done
 else
