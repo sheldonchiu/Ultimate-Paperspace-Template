9dc131020d0c026fa7282d04375a144bc4cffbc4
sheldon <sheldonxxx@pm.me>
2023-03-30 15:47:57 +0800

    fix

--- cloudflared/main.sh
+++ cloudflared/main.sh
@@ -27,7 +27,7 @@ if [ "${CF_TOKEN}" = "quick" ]; then
         retries=0
         max_retries=10
         while true; do
-            response=$(curl  http://localhost:${metrics_port}/quicktunnel || true)
+            response=$(curl http://localhost:${metrics_port}/quicktunnel || true)
             if [ $? -eq 0 ] && [ "$(echo "$response" | jq -r '.hostname')" != "null" ]; then
                 hostname=$(echo "$response" | jq -r '.hostname')
                 echo "Success! Hostname is $hostname"
