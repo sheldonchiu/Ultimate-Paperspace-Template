4a084af823a2bb32a28a3e20f9c1699f185ee54a
Xxxx <you@example.com>
2023-04-09 22:29:45 +0800

    update

---
+++ cloudflare_reload.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function source_env_file() {
+  if [ -e ".env" ]; then
+    source ".env"
+  fi
+}
+
+function check_required_env_vars() {
+  local required_vars=($(echo "$REQUIRED_ENV" | tr ',' '\n'))
+  local missing_vars=()
+  for var in "${required_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+      missing_vars+=("$var")
+    fi
+  done
+  if [ ${#missing_vars[@]} -gt 0 ]; then
+    echo "The following required environment variables are missing: ${missing_vars[*]}"
+    return 1
+  fi
+  return 0
+}
+
+export SCRIPT_ROOT_DIR=$(dirname "$(realpath "$0")")
+cd $SCRIPT_ROOT_DIR
+
+# Read the RUN_SCRIPT environment variable
+run_script="$RUN_SCRIPT"
+
+# Separate the variable by commas
+IFS=',' read -ra scripts <<< "$run_script"
+
+# Loop through each script and execute the corresponding case
+for script in "${scripts[@]}"
+do
+  cd $SCRIPT_ROOT_DIR
+  if [ ! -d "$script" ]; then
+    bash utils/discord/send.sh "Script folder $script not found, skipping..."
+    continue
+  fi
+  cd $script
+  source_env_file
+done
+
+bash cloudflared/control.sh reload
