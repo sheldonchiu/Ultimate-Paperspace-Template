813566569670604417a04b1da912370afb56bbc7
Xxxx <you@example.com>
2023-04-13 00:03:53 +0800

    update textgen

--- textgen/main.sh
+++ textgen/main.sh
@@ -44,8 +44,6 @@ if ! [[ -e "/tmp/textgen.prepared" ]]; then
     TARGET_REPO_DIR=$REPO_DIR/repositories/GPTQ-for-LLaMa \
     TARGET_REPO_BRANCH="cuda" \
     TARGET_REPO_URL="https://github.com/qwopqwop200/GPTQ-for-LLaMa.git" \
-    # UPDATE_REPO=$TEXTGEN_UPDATE_REPO \
-    # UPDATE_REPO_COMMIT=$TEXTGEN_UPDATE_REPO_COMMIT \
     bash $current_dir/../utils/prepare_repo.sh
 
     # git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa.git -b cuda
@@ -64,30 +62,43 @@ echo "Finished setting up Text generation Webui"
 args=""
 echo "Downloading Models for Text generation Webui..."
 mkdir -p $REPO_DIR/models
-IFS=',' read -ra models <<< "$FASTCHAT_MODEL"
+IFS=',' read -ra models <<< "$TEXTGEN_MODEL"
 for model in "${models[@]}"
 do
     cd /tmp
     if [[ "$model" == "vicuna-7b" ]]; then
-        git clone https://huggingface.co/sheldonxxxx/llama-vicuna-7b
-        create_symlink /tmp/llama-vicuna-7b $REPO_DIR/models/llama-vicuna-7b
+        SYMLINKS=("/tmp/llama-vicuna-7b:$REPO_DIR/models/llama-vicuna-7b") \
+        TARGET_REPO_DIR="/tmp/llama-vicuna-7b" \
+        TARGET_REPO_BRANCH="main" \
+        TARGET_REPO_URL="https://huggingface.co/sheldonxxxx/llama-vicuna-7b" \
+        bash $current_dir/../utils/prepare_repo.sh
         model_name="llama-vicuna-7b"
     elif [[ "$model" == "vicuna-13b" ]]; then
-        git clone https://huggingface.co/eachadea/vicuna-13b
-        create_symlink /tmp/vicuna-13b $REPO_DIR/models/vicuna-13b
+        SYMLINKS=("/tmp/llama-vicuna-13b:$REPO_DIR/models/vicuna-13b") \
+        TARGET_REPO_DIR="/tmp/vicuna-13b" \
+        TARGET_REPO_BRANCH="main" \
+        TARGET_REPO_URL="https://huggingface.co/eachadea/vicuna-13b" \
+        bash $current_dir/../utils/prepare_repo.sh
         model_name="vicuna-13b"
         args="--load-in-8bit"
     elif [[ "$model" == "vicuna-13b-GPTQ-4bit-128g" ]]; then
-        git clone https://huggingface.co/anon8231489123/vicuna-13b-GPTQ-4bit-128g
-        create_symlink /tmp/vicuna-13b-GPTQ-4bit-128g $REPO_DIR/models/vicuna-13b-GPTQ-4bit-128g
+        SYMLINKS=("/tmp/vicuna-13b-GPTQ-4bit-128g:$REPO_DIR/models/vicuna-13b-GPTQ-4bit-128g") \
+        TARGET_REPO_DIR="/tmp/vicuna-13b-GPTQ-4bit-128g" \
+        TARGET_REPO_BRANCH="main" \
+        TARGET_REPO_URL="https://huggingface.co/anon8231489123/vicuna-13b-GPTQ-4bit-128g" \
+        bash $current_dir/../utils/prepare_repo.sh
         model_name="vicuna-13b-GPTQ-4bit-128g"
         args="--wbits 4 --groupsize 128"
     elif [[ "$model" == "vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g" ]]; then
-        GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/TheBloke/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g/
+        GIT_LFS_SKIP_SMUDGE=1 \
+        SYMLINKS=("/tmp/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g:$REPO_DIR/models/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g") \
+        TARGET_REPO_DIR="/tmp/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g" \
+        TARGET_REPO_BRANCH="main" \
+        TARGET_REPO_URL="https://huggingface.co/TheBloke/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g/" \
+        bash $current_dir/../utils/prepare_repo.sh
         cd vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g
         wget https://huggingface.co/TheBloke/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g/resolve/main/tokenizer.model
         wget https://huggingface.co/TheBloke/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g/resolve/main/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g.safetensors
-        create_symlink /tmp/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g $REPO_DIR/models/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g
         model_name="vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g"
         args="--wbits 4 --groupsize 128"
     fi
@@ -96,7 +107,7 @@ echo "Finished downloading Models for Text generation Webui"
 
 echo "Starting Text generation Webui..."
 cd $REPO_DIR
-nohup python server.py --model model_name $args > /tmp/textgen.log 2>&1 &
+nohup python server.py  --listen-port $TEXTGEN_PORT --model $model_name $args > /tmp/textgen.log 2>&1 &
 echo $! > /tmp/textgen.pid
 
 echo "Text generation Webui started"
