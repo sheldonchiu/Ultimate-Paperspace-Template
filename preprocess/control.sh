041af16d42b7112c9eab181225d2984e9f90ef3b
sheldonchiu <sheldon.chiu1998@gmail.com>
2023-07-29 23:33:25 +0800

    test preprocess

---
+++ preprocess/control.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+current_dir=$(dirname "$(realpath "$0")")
+cd $current_dir
+source .env
+
+# Set up a trap to call the error_exit function on ERR signal
+trap 'error_exit "### ERROR ###"' ERR
+
+echo "### Command received ###"
+file="/tmp/preprocess.pid"
+if [[ $1 == "reload" ]]; then
+    log "Reloading preprocess"
+    
+    kill_pid $file
+    # Wait for 1s to avoid unexpected behavior
+    sleep 1
+    bash main.sh
+    
+elif [[ $1 == "start" ]]; then
+    log "Starting preprocess"
+    
+    bash main.sh
+    
+elif [[ $1 == "stop" ]]; then
+    log "Stopping preprocess"
+        
+    kill_pid $file
+    
+
+else
+  echo "Invalid argument"
+fi
+
+echo "### Done ###"
---
+++ preprocess/main.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+current_dir=$(dirname "$(realpath "$0")")
+cd $current_dir
+source .env
+
+# Set up a trap to call the error_exit function on ERR signal
+trap 'error_exit "### ERROR ###"' ERR
+
+
+echo "### Setting up preprocess ###"
+log "Setting up preprocess"
+TARGET_REPO_DIR=$PREPROCESS_REPO_DIR \
+TARGET_REPO_BRANCH="main" \
+TARGET_REPO_URL="https://github.com/sheldonchiu/paperspace-sd-auto-preprocess.git" \
+bash $current_dir/../utils/prepare_repo.sh
+
+TARGET_REPO_DIR=$TRAINER_REPO_DIR \
+TARGET_REPO_BRANCH="sdxl" \
+TARGET_REPO_URL="https://github.com/sheldonchiu/kohya-trainer-paperspace.git" \
+bash $current_dir/../utils/prepare_repo.sh  
+if ! [[ -e "/tmp/preprocess.prepared" ]]; then
+    
+    
+    python3 -m venv /tmp/preprocess-env
+    
+    source /tmp/preprocess-env/bin/activate
+
+    pip install --upgrade pip
+    pip install --upgrade wheel setuptools
+    
+    cd $PREPROCESS_REPO_DIR/preprocess
+    bash prepare_env.sh
+    
+    touch /tmp/preprocess.prepared
+else
+    
+    source /tmp/preprocess-env/bin/activate
+    
+fi
+log "Finished Preparing Environment for preprocess"
+
+
+
+cd $current_dir/../kosmos2
+bash control.sh stop
+
+echo "### Starting preprocess ###"
+log "Starting preprocess"
+cd $PREPROCESS_REPO_DIR/preprocess
+
+bash run.sh > $LOG_DIR/preprocess.log 2>&1 &
+echo $! > /tmp/preprocess.pid
+
+send_to_discord "preprocess Started"
+
+echo "### Done ###"
