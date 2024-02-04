ba8bfca2e94cab348f209e2578fd8b590925ec84
sheldonchiu <sheldon.chiu1998@gmail.com>
2024-02-04 22:27:20 +0900

    add rclone cache arg

--- rclone/main.sh
+++ rclone/main.sh
@@ -34,7 +34,7 @@ log "Finished Preparing Environment for Rclone"
 if [[ -z "$INSTALL_ONLY" ]]; then
   echo "### Starting Rclone ###"
   log "Starting Rclone"
-  rclone serve $RCLONE_SERVE_PROTOCOL --addr :$RCLONE_PORT --copy-links --user $RCLONE_USERNAME --pass $RCLONE_PASSWORD $RCLONE_SERVE_PATH ${EXTRA_RCLONE_ARGS} > $LOG_DIR/rclone_serve.log 2>&1 &
+  rclone serve $RCLONE_SERVE_PROTOCOL --dir-cache-time 10 --addr :$RCLONE_PORT --copy-links --user $RCLONE_USERNAME --pass $RCLONE_PASSWORD $RCLONE_SERVE_PATH ${EXTRA_RCLONE_ARGS} > $LOG_DIR/rclone_serve.log 2>&1 &
   echo $! > /tmp/rclone.pid
 fi
 
